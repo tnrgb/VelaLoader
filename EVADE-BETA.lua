38150281787e8bb65d332a20ec8c11dc1f6e3579
tnrgb <tn7639318@gmail.com>
2026-07-25 16:35:04 +0700

    Update EVADE-BETA.lua

--- EVADE-BETA.lua
+++ EVADE-BETA.lua
@@ -5,12 +5,12 @@ end
 -- 1. Load UI lên đầu tiên
 load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/ui.lua")
 
-task.wait(100)
+task.wait(50)
 
 -- 2. Load Auto Join thứ hai
 load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/autojoin")
 
-task.wait(30)
+task.wait(20)
 
 -- 3. Load Main cuối cùng
 load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/main.lua")
