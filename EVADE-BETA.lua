c0faf14063b1b68f3295db881f31cf31eea6da00
tnrgb <tn7639318@gmail.com>
2026-07-25 16:23:49 +0700

    Update EVADE-BETA.lua

--- EVADE-BETA.lua
+++ EVADE-BETA.lua
@@ -1,14 +1,13 @@
-local function load(url)
-    return loadstring(game:HttpGet(url))()
+local function loadAsync(url)
+    task.spawn(function()
+        pcall(function()
+            loadstring(game:HttpGet(url))()
+        end)
+    end)
 end
 
--- Tải Auto Join trước và chờ ổn định
-load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/autojoin")
-task.wait(1) -- Chờ 1 giây cho autojoin chạy xong logic ban đầu
-
--- Tải Main script
-load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/main.lua")
-task.wait(0.8)
-
--- Tải UI cuối cùng
-load("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/ui.lua")
+loadAsync("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/autojoin")
+task.wait(0.5)
+loadAsync("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/main.lua")
+task.wait(0.5)
+loadAsync("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/ui.lua")
