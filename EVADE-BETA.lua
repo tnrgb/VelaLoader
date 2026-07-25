956349c08c19b64f78354d9661b53fe77a0949ae
tnrgb <tn7639318@gmail.com>
2026-07-25 13:46:09 +0700

    Add EVADE-BETA.lua for loading scripts

---
+++ EVADE-BETA.lua
@@ -0,0 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/main.lua"))()
+
+task.wait(0.2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/ui.lua"))()
