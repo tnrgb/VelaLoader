0afb643d5205d2a605e54ac04685e7c4bc8d331a
tnrgb <tn7639318@gmail.com>
2026-07-26 13:12:15 +0700

    Update EVADE-BETA.lua

--- EVADE-BETA.lua
+++ EVADE-BETA.lua
@@ -15,13 +15,13 @@ task.spawn(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/ui.lua"))()
     end)
 
-    task.wait(7)
+    task.wait(5.5)
 
     pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/autojoin"))()
     end)
 
-    task.wait(4)
+    task.wait(2)
 
     pcall(function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/tnrgb/VelaLoader/refs/heads/main/main.lua"))()
